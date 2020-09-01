bfe0d38a864a4d4de0456137160821149ef2fc81
Mordechai Botrashvily <Mordechai.Botrashvily@elbitsystems.com>
2020-09-01 15:59:11 +0300

    - Removed unnecessary imports and organized. - Removed conversion from C# array since now using native pointer from shmem as input, so image is numpy native. - Took deep sort function after Amit change.

--- yolov3_deepsort_headless.py
+++ yolov3_deepsort_headless.py
@@ -1,12 +1,8 @@
 import os
-from os.path import dirname, abspath, join
-import time
-import argparse
-import torch
+from os.path import dirname, join
+
 import numpy as np
-import sys
-import ctypes
-from distutils.util import strtobool
+import torch
 
 from detector import build_detector
 from deep_sort import build_tracker
@@ -49,78 +45,22 @@ class Tracker:
         self.target_id = []
 
     def DeepSort(self, im, target_cls):
-        im = self.asNumpyArray(im)
-        im = im.reshape(720, 1280, 3)
-
+        im = im.reshape(self.args.img_height, self.args.img_width, 3)
         # do detection
         bbox_xywh, cls_conf, cls_ids = self.detector(im)    # get all detections from image
-        outputs = []
+        tracks = []
         detections = []
-        detections_conf = []
-        cls_id = []
-        target_xyz = []
 
-        if bbox_xywh is not None:
-            # select person class
-            mask = np.isin(cls_ids[0], list(self.cls_dict.keys()))
+        # select person class
+        mask = np.isin(cls_ids[0], list(self.cls_dict.keys()))
+        bbox_xywh = bbox_xywh[0][mask]
+        cls_conf = cls_conf[0][mask]
+        cls_ids = cls_ids[0][mask]
 
-            bbox_xywh = bbox_xywh[0][mask]
-            cls_conf = cls_conf[0][mask]
-            cls_ids = cls_ids[0][mask]
-
-            # run deepsort algorithm to match detections to tracks
-            outputs, detections, detections_conf, cls_id = self.deepsort.update(bbox_xywh, cls_conf, cls_ids, im)
-
+        if len(cls_ids) > 0:
+            tracks, detections = self.deepsort.update(bbox_xywh, cls_conf, cls_ids, im)
             # calculate object distance and direction from camera
-            target_xyz = []
-            for i, target in enumerate(outputs):
-                target_xyz.append(self.cam2world.obj_world_xyz(x1=target[0],
-                                                               y1=target[1],
-                                                               x2=target[2],
-                                                               y2=target[3],
-                                                               obj_height_meters=self.args.target_height))
+            for i, track in enumerate(tracks):
+                track.to_xyz(self.cam2world, obj_height_meters=self.args.target_height)
 
-        cls_name = [self.cls_dict[int(target_cls)] for target_cls in cls_id]
-
-        return outputs, detections, detections_conf, target_xyz, cls_name, len(outputs)
-
-
-    def asNumpyArray(self, netArray):
-        '''
-        Given a CLR `System.Array` returns a `numpy.ndarray`.  See _MAP_NET_NP for
-        the mapping of CLR types to Numpy dtypes.
-        '''
-
-        _MAP_NET_NP = {
-        'Single' : np.dtype('float32'),
-        'Double' : np.dtype('float64'),
-        'SByte'  : np.dtype('int8'),
-        'Int16'  : np.dtype('int16'),
-        'Int32'  : np.dtype('int32'),
-        'Int64'  : np.dtype('int64'),
-        'Byte'   : np.dtype('uint8'),
-        'UInt16' : np.dtype('uint16'),
-        'UInt32' : np.dtype('uint32'),
-        'UInt64' : np.dtype('uint64'),
-        'Boolean': np.dtype('bool'),
-    }
-
-
-        dims = np.empty(netArray.Rank, dtype=int)
-        for I in range(netArray.Rank):
-            dims[I] = netArray.GetLength(I)
-        netType = netArray.GetType().GetElementType().Name
-
-        try:
-            npArray = np.empty(dims, order='C', dtype=_MAP_NET_NP[netType])
-        except KeyError:
-            raise NotImplementedError("asNumpyArray does not yet support System type {}".format(netType) )
-
-        try: # Memmove
-            sourceHandle = GCHandle.Alloc(netArray, GCHandleType.Pinned)
-            sourcePtr = sourceHandle.AddrOfPinnedObject().ToInt64()
-            destPtr = npArray.__array_interface__['data'][0]
-            ctypes.memmove(destPtr, sourcePtr, npArray.nbytes)
-        finally:
-            if sourceHandle.IsAllocated: sourceHandle.Free()
-        return npArray
+        return tracks, detections
